34ff63f5cb0b7543eedb87d085868bc924d8a9dd
Nathaniel Sletten <ns09slet@siena.edu>
2023-02-23 14:30:28 -0500

    Updated description of the class

--- Portfolio.java
+++ Portfolio.java
@@ -1,109 +1,135 @@
 import java.util.ArrayList;
 /**
- * Create a portfolio of stocks.
+ * The class Portfolio is a Java implementation of a financial portfolio, which manages a collection of StockHolding objects. 
+ * It provides methods for buying and selling stocks, 
+ * tracking the lifetime investment and payout, and calculating the current value of the portfolio.
  *
  * @author (Matthew Laframboise, Nathaniel Sletten)
  * @version (2/22/2023)
  */
-public class Portfolio
-{
-    // Array list of stocks
+public class Portfolio{
     private ArrayList<StockHolding> stocks;
-    private double lifeTimeMade;
-    private double payout;
+    private double lifetimeInvestment;
+    private double lifetimePayout;
 
-    // Default constructor
-    public Portfolio(){
-        stocks = new ArrayList<StockHolding>();
-        lifeTimeMade = 0.0;
-        payout = 0.0;
-    }
-
-    // Add accessor methods
-    public double getLifeTimeMade(){
-        return lifeTimeMade;
-    }
-
-    public double getPayout(){
-        return payout;
-    }
-
-    // Private method to get index of a stock in the ArrayList by symbol
-    private int getIndex(String symbol){
-        for(int i=0; i<stocks.size(); i++){
-            if(stocks.get(i).getSymbol().equals(symbol)){
-                return i;
-            }
-        }
-        return -1;
-    }
-
-    // Method to buy a stock
+    //constructor
     /**
-     * Buys the given number of shares of a stock at the given price per share. If the stock
-     * already exists in the portfolio, the number of shares is added to the existing holding.
-     * Otherwise, a new StockHolding object is added to the ArrayList.
-     * @param symbol the symbol of the stock to buy
-     * @param name the name of the stock to buy
-     * @param shares the number of shares to buy
-     * @param pricePerShare the current price per share of the stock
-     * @return the cost of buying the shares, i.e., the number of shares bought times the price per share
+     * Constructs the ArrayList of StockHolding objects to represent a portfolio that contains the number of stocks bought or sold
+     * and initializes lifetimeInvestment, sum of all the stock purchases made, and lifetimePayout,sum of all the stock purchases 
+     * sold,to 0. 
+     * 
      */
-    public double buyStock(String symbol, String name, int shares, double pricePerShare) {
-        double cost = shares * pricePerShare;
-        int index = getIndex(symbol);
-        if (index == -1) {
-            // Stock not found in portfolio, add a new StockHolding
-            StockHolding stock = new StockHolding(symbol, name, shares, pricePerShare);
-            stocks.add(stock);
-        } else {
-            // Stock found in portfolio, update existing StockHolding
-            StockHolding stock = stocks.get(index);
-            stock.buyShares(shares, pricePerShare);
+    public Portfolio( ){
+        stocks = new ArrayList<StockHolding>();
+        lifetimeInvestment = 0.0;
+        lifetimePayout = 0.0;
+    }
+
+    /**
+     * Returns the lifetimeInvestment of the portfolio.
+     * 
+     * @return The lifetimeInvestment of the portfolio
+     */
+    public double getLifetimeInvestment(){
+        return lifetimeInvestment;
+    }
+
+    /**
+     * Returns the lifetimePayout of the portfolio 
+     * 
+     * @return The lifetimePayout of the portfolio 
+     */
+    public double getLifetimePayout(){
+        return lifetimePayout;
+    }
+
+    /**
+     * This method takes as input the stock symbol and returns the index of the stock in stocks that has the same stock symbol as
+     * the input. Returns -1, if no stock was found in stocks .
+     * 
+     * @return getIndex The index of the stock in stocks that has the same stock symbol as input or -1 if no stock was not found
+     * @param symbol The stock symbol to search for in stocks
+     * 
+     */
+    private int getIndex(String symbol) {
+        int index = 0; 
+        int var = -1;
+        for(StockHolding s : stocks)
+        {
+            if(stocks.get(index).getSymbol().equals(symbol))
+            {
+                var = index;
+            }
+            index++;
         }
-        lifeTimeMade += cost;
+        return var; 
+    }
+
+    /**
+     * This method takes as input the stock symbol, name of stock, number of shares to
+     * buy and the current price per share. If index of input symbol is in stocks, input numShares 
+     */
+    public double buyStock( String symbol, String stockName, int numShares, double price )
+    {
+        int found = getIndex(symbol);
+        StockHolding sh = new StockHolding (symbol,stockName, numShares, price);
+        double cost = 0.0;
+        if(found > -1)
+        {
+            stocks.get(found).buyShares(numShares, price);
+            cost = numShares * price;
+            lifetimeInvestment += cost;
+        }
+
+        else
+        {
+            stocks.add(sh);
+        }
+
         return cost;
     }
 
-    /**
-     * Sells the given number of shares of a stock from the portfolio. If the number of shares sold
-     * results in a holding of zero, the StockHolding object is removed from the ArrayList.
-     * @param symbol the symbol of the stock to sell
-     * @param shares the number of shares to sell
-     * @return the payout from selling the shares, i.e., the number of shares sold times the price per share
-     */
-    public double sellStock(String symbol, int shares) {
+    public double sellStock (String symbol, int numShares)
+    {
         int index = getIndex(symbol);
-        if (index != -1) {
-            // Stock found in portfolio, sell shares
-            StockHolding stock = stocks.get(index);
-            double payout = stock.sellShares(shares);
-            if (stock.getNumShares() == 0) {
-                // Remove StockHolding object from ArrayList if no shares remain
+        double profit = 0.0;
+        if(index != -1)
+        {
+            profit = stocks.get(index).sellShares(numShares);
+            if(stocks.get(index).getNumShares()==0)
+            {
                 stocks.remove(index);
             }
-            this.payout += payout;
-            return payout;
         }
-        return 0;
+
+        lifetimePayout += profit;
+
+        return profit;
+    }
+
+    public double getCurrentValue()
+    {
+
+        int index = 0;
+        double value = 0.0;
+        for(StockHolding s: stocks)
+        {
+            value += stocks.get(index).getNumShares() * stocks.get(index).getPrice();
+            index++;
+        }
+
+        return value;
+
     }
 
     /**
-     * Returns the current value of the portfolio, which is the sum of the number of shares times the
-     * price per share for each stock in the portfolio.
-     * @return the current value of the portfolio
+     * 
      */
-    public double getCurrentValue() {
-        double value = 0.0;
-        for (StockHolding stock : stocks) {
-            value += stock.getNumShares() * stock.getPrice();
-        }
-        return value;
-    }
-
     @Override
-    public String toString(){
-        //DO NOT EDIT THIS METHOD.
+
+    public String toString()
+    {
+        // //DO NOT EDIT THIS METHOD.
         StringBuffer sb = new StringBuffer();
         sb.append(String.format("%6s%25s%10s%15s%15s%n", 
                 "Symbol", "Name", "Shares", "@Price", "Total"));
--- PortfolioReport.java
+++ PortfolioReport.java
@@ -1,26 +1,17 @@
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
-
 /**
  * This program creates a portfolio by reading the transactions in
  * the file transactions.txt and buying and selling stock shares
  * accordingly.  Then, this program prints the portfolio report
  * as described in the problem set 2 instructions.
  *
- * @author 
+ * @Nathaniel Sletten 
  * @version 
  */
 public class PortfolioReport
 {
-    /**
-     * This program creates a portfolio by reading the transactions in
-     * the file transactions.txt and buying and selling stock shares
-     * accordingly.  Then, this program prints the portfolio report
-     * as described in the problem set 2 instructions.
-     * 
-     * @param args No command line arguments required.
-     */
     public static void main(String[] args) throws FileNotFoundException
     {
         Scanner file = new Scanner(new File("transactions.txt"));        
@@ -29,21 +20,26 @@ public class PortfolioReport
         while(file.hasNext()){
             String line = file.nextLine();
             String[] data = line.split(",");
+
             if(data[0].equals("B")){
                 //Buy the stock for the portfolio.
-                //YOUR CODE HERE.
-            } else {
+                String numSharesBuy = data[3];
+                String price = data[4];
+                p.buyStock(data[1],data[2],Integer.parseInt(numSharesBuy.trim()),Double.parseDouble(price));
+            } else{ 
                 //Sell the stock from the portfolio.
-                //YOUR CODE HERE.
+                String numSharesSell = data[2];
+                p.sellStock(data[1],Integer.parseInt(numSharesSell.trim()));
             }
-        }
 
-        System.out.print(p);
-        System.out.println();
-        
-        // Uncomment the lines of code below and complete the print statements to work as intended.
-        // System.out.println(String.format("      Current Value:  $%,15.2f", //YOUR CODE HERE.));
-        // System.out.println(String.format("Lifetime Investment:  $%,15.2f", //YOUR CODE HERE.));
-        // System.out.println(String.format("    Lifetime Payout:  $%,15.2f", //YOUR CODE HERE.));
+            System.out.print(p);
+            System.out.println();
+
+            // Uncomment the lines of code below and complete the print statements to work as intended.
+            System.out.println(String.format("      Current Value:  $%,15.2f", p.getCurrentValue()));
+            System.out.println(String.format("Lifetime Investment:  $%,15.2f",p.getLifetimeInvestment()));
+            System.out.println(String.format("    Lifetime Payout:  $%,15.2f", p.getLifetimePayout()));
+        }
     }
 }
+
