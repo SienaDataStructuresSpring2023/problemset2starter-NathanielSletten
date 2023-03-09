f6c2d5404a2438529dc086a0412ba0dd1eb1cd87
MatthewLaframboise <md17lafr@siena.edu>
2023-03-09 17:08:03 -0500

    Removed single line comments

--- PortfolioReport.java
+++ PortfolioReport.java
@@ -14,25 +14,19 @@ public class PortfolioReport
 {
     public static void main(String[] args) throws FileNotFoundException
     {
-        // create a new scanner object
         Scanner file = new Scanner(new File("transactions.txt"));
-        
-        // construct a new Portfolio object
+    
         Portfolio p = new Portfolio();
         
-        // create a while loop that checks if the file has data, and assign the instance variables the correct values 
-        //according to their position in the array "data"
         while(file.hasNext()){
             String line = file.nextLine();
             String[] data = line.split(",");
 
             if(data[0].equals("B")){
-                //Buy the stock for the portfolio.
                 String numSharesBuy = data[3];
                 String price = data[4];
                 p.buyStock(data[1],data[2],Integer.parseInt(numSharesBuy.trim()),Double.parseDouble(price));
             } else{ 
-                //Sell the stock from the portfolio.
                 String numSharesSell = data[2];
                 p.sellStock(data[1],Integer.parseInt(numSharesSell.trim()));
             }
@@ -40,7 +34,6 @@ public class PortfolioReport
             System.out.print(p);
             System.out.println();
 
-            // Uncomment the lines of code below and complete the print statements to work as intended.
             System.out.println(String.format("      Current Value:  $%,15.2f", p.getCurrentValue()));
             System.out.println(String.format("Lifetime Investment:  $%,15.2f",p.getLifetimeInvestment()));
             System.out.println(String.format("    Lifetime Payout:  $%,15.2f", p.getLifetimePayout()));
